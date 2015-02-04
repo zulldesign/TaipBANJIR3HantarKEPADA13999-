97d562547ce7bf7053fd659003ffafc6d3fea443
zulldesign <admin@zulldesign.ml>
2015-02-04 23:34:54 +0800

    l

--- connect_to_mysql.php
+++
@@ -1,5 +0,0 @@
-<?php 
-// place your DB_host, UserName, Password, and DB_Name below where shown
-mysql_connect("ba5d0da1-2181-4794-b45c-a41c00885ae9.mysql.sequelizer.com","wnpljvjmiarfmdel","wmgWrYyftvchv6vPmrYKXHBC5v7sEaN2Tsfk6imdfFiaWogwNbcmrGKVSLZ4ZgGt") or die ("Could not connect.");
-mysql_select_db("dbba5d0da121814794b45ca41c00885ae9") or die ("no database");
-?>
